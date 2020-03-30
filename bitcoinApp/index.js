deacc2876bf92bcb62a076e618c3fc42fda0baa6
松本雄揮 <yukiji.0307@gmail.com>
2020-03-30 12:32:46 +0900

    bitCoinApp

---
+++ bitcoinApp/index.js
@@ -0,0 +1,35 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    bpi: null,
+    hasError: false,
+    loading: true
+  },
+  mounted() {
+    axios
+      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+      .then(
+        function(response) {
+          // console.log(response.data.bpi)
+          // console.log(response.data.bpi.USD.rate_float)
+          this.bpi = response.data.bpi
+        }.bind(this)
+      )
+      .catch(
+        function(error) {
+          console.log(error)
+          this.hasError = true
+        }.bind(this)
+      )
+      .finally(
+        function() {
+          this.loading = false
+        }.bind(this)
+      )
+  },
+  filters: {
+    currencyDecimal(value) {
+      return value.toFixed(2)
+    }
+  }
+})
