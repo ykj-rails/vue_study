017a74d22f70a65b2439277896b686a0f9d22c71
松本雄揮 <yukiji.0307@gmail.com>
2020-04-18 12:21:03 +0900

    vue-cli app create

---
+++ vue-cli-project/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <HelloNuxt />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloNuxt from '@/components/HelloNuxt.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloNuxt
+  }
+}
+</script>
